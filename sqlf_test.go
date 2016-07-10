41cc3f6600c423d1aa3bde5f20b32c1d99b3da37
John Jeffery <jjeffery@sp.com.au>
2016-07-10 20:43:43 +1000

    Implementation of SelectRowStmt

--- sqlf_test.go
+++ sqlf_test.go
@@ -45,12 +45,10 @@ type User struct {
 	UpdatedAt time.Time
 }
 
-func Test1(t *testing.T) {
+func TestInsertUpdate(t *testing.T) {
 	setup(t)
 	table := sqlf.Table("users", User{})
-	insertStmt := sqlf.InsertRowPrintf(`
-	insert into %s(%s) 
-	values(%s)`,
+	insertStmt := sqlf.InsertRowPrintf(`insert into %s(%s) values(%s)`,
 		table.Insert.TableName,
 		table.Insert.Columns,
 		table.Insert.Values)
@@ -59,13 +57,13 @@ func Test1(t *testing.T) {
 	defer tx.Rollback()
 	checkError(t, err, "cannot begin tx")
 
-	u := &User{Name: "Name", UpdatedAt: time.Now()}
+	u1 := &User{Name: "Name", UpdatedAt: time.Now()}
 
-	err = insertStmt.Exec(tx, u)
+	err = insertStmt.Exec(tx, u1)
 	checkError(t, err, "cannot insert")
 
-	if u.ID != 1 {
-		t.Errorf("expected=1, actual=%d", u.ID)
+	if u1.ID != 1 {
+		t.Errorf("expected=1, actual=%d", u1.ID)
 	}
 
 	updateStmt := sqlf.UpdateRowPrintf(`update %s set %s where %s`,
@@ -74,13 +72,48 @@ func Test1(t *testing.T) {
 		table.Update.WhereColumns)
 	t.Logf("update query: %s", updateStmt.Query())
 
-	u.Name = "Another name"
-	u.UpdatedAt = time.Now()
+	u1.Name = "Another name"
+	u1.UpdatedAt = time.Now()
 	var rowsAffected int
-	rowsAffected, err = updateStmt.Exec(tx, u)
+	rowsAffected, err = updateStmt.Exec(tx, u1)
 	checkError(t, err, "cannot update")
 	if rowsAffected != 1 {
-		t.Errorf("rowsAffected: expected=1, actual=%d", rowsAffected)
+		t.Errorf("rows updated: expected=1, actual=%d", rowsAffected)
+	}
+
+	u2 := &User{Name: "User2", UpdatedAt: time.Now()}
+	err = insertStmt.Exec(tx, u2)
+	checkError(t, err, "cannot insert")
+	if u2.ID != 2 {
+		t.Errorf("expected=2, actual=%d", u2.ID)
+	}
+
+	selectStmt := sqlf.SelectRowsPrintf("select %s from %s order by %s",
+		table.Select.Columns,
+		table.Select.TableName,
+		table.Select.OrderBy)
+
+	var users []*User
+	err = selectStmt.Select(tx, &users)
+	checkError(t, err, "cannot select")
+
+	if len(users) != 2 {
+		t.Errorf("select: expected rows=2, actual=%d", len(users))
+	}
+	t.Log("users: ", users)
+	for i, u := range users {
+		t.Logf("user %d: %v", i+1, u)
+	}
+
+	deleteStmt := sqlf.UpdateRowPrintf(`delete from %s where %s`,
+		table.Delete.TableName,
+		table.Delete.WhereColumns)
+	t.Logf("deltete query: %s", deleteStmt.Query())
+
+	rowsAffected, err = deleteStmt.Exec(tx, u1)
+	checkError(t, err, "cannot delete")
+	if rowsAffected != 1 {
+		t.Errorf("rows deleted: expected=1, actual=%d", rowsAffected)
 	}
 
 	checkError(t, tx.Commit(), "cannot commit")
--- stmt.go
+++ stmt.go
@@ -12,11 +12,30 @@ import (
 	"github.com/jjeffery/sqlf/private/scanner"
 )
 
+// commonStmt contains fields and methods common to
+// insert, update, delete and select statements.
+type commonStmt struct {
+	query string
+	table *TableInfo
+	err   error
+}
+
+func (stmt commonStmt) expectedTypeString() string {
+	return fmt.Sprintf("%s.%s", stmt.table.rowType.PkgPath(), stmt.table.rowType.Name())
+}
+
+func (stmt commonStmt) expectTable() error {
+	if stmt.table == nil {
+		return errors.New("cannot determine row type from query")
+	}
+	return nil
+}
+
+// execRowStmt contains fields and methods common to insert,
+// updated and delete row statements.
 type execRowStmt struct {
-	query  string
-	table  *TableInfo
+	commonStmt
 	inputs []*column.Info
-	err    error
 }
 
 func (stmt execRowStmt) getArgs(row interface{}) ([]interface{}, error) {
@@ -41,6 +60,12 @@ func (stmt execRowStmt) getArgs(row interface{}) ([]interface{}, error) {
 }
 
 func (stmt execRowStmt) doExec(db Execer, row interface{}) (sql.Result, error) {
+	if stmt.err != nil {
+		return nil, stmt.err
+	}
+	if err := stmt.expectTable(); err != nil {
+		return nil, err
+	}
 	args, err := stmt.getArgs(row)
 	if err != nil {
 		return nil, err
@@ -61,7 +86,7 @@ func (stmt execRowStmt) getRowValue(row interface{}) (reflect.Value, error) {
 }
 
 func (stmt execRowStmt) errorType() error {
-	expectedType := fmt.Sprintf("%s.%s", stmt.table.rowType.PkgPath(), stmt.table.rowType.Name())
+	expectedType := stmt.expectedTypeString()
 	return fmt.Errorf("expected type %s or *(%s)", expectedType, expectedType)
 }
 
@@ -171,17 +196,114 @@ func UpdateRowPrintf(format string, args ...interface{}) *UpdateRowStmt {
 
 	for _, arg := range args {
 		if tn, ok := arg.(TableName); ok {
-			if tn.clause == clauseUpdateTable {
+			if tn.clause.matchAny(clauseUpdateTable, clauseDeleteTable) {
 				stmt.table = tn.table
 			}
-		}
-		if cols, ok := arg.(Columns); ok {
-			if stmt.table == nil && (cols.clause == clauseUpdateSet || cols.clause == clauseUpdateWhere) {
+		} else if cols, ok := arg.(Columns); ok {
+			if stmt.table == nil && cols.clause.matchAny(clauseUpdateSet, clauseUpdateWhere, clauseDeleteWhere) {
 				stmt.table = cols.table
 			}
 			if cols.clause.isInput() {
 				// input parameters for the UPDATE statement
 				stmt.inputs = append(stmt.inputs, cols.filtered()...)
+			}
+		}
+	}
+
+	// generate the SQL statement and scan it
+	stmt.query = fmt.Sprintf(format, args...)
+	stmt.query = scanSQL(stmt.table.dialect, stmt.query)
+	return stmt
+}
+
+type SelectStmt struct {
+	commonStmt
+	columns []*column.Info
+}
+
+func (stmt *SelectStmt) Select(db Queryer, dest interface{}, args ...interface{}) error {
+	if stmt.err != nil {
+		return stmt.err
+	}
+	if err := stmt.expectTable(); err != nil {
+		return err
+	}
+	destValue := reflect.ValueOf(dest)
+
+	if destValue.Kind() != reflect.Ptr {
+		return stmt.errorSliceType()
+	}
+	if destValue.IsNil() {
+		return errors.New("Select: nil pointer passed as dest")
+	}
+
+	sliceValue := reflect.Indirect(destValue)
+	sliceType := sliceValue.Type()
+	if sliceType.Kind() != reflect.Slice {
+		return stmt.errorSliceType()
+	}
+
+	rowType := sliceType.Elem()
+	isPtr := rowType.Kind() == reflect.Ptr
+	if isPtr {
+		rowType = rowType.Elem()
+	}
+	if rowType != stmt.table.rowType {
+		return stmt.errorSliceType()
+	}
+
+	rows, err := db.Query(stmt.query, args...)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	scanValues := make([]interface{}, len(stmt.columns))
+
+	for rows.Next() {
+		rowValuePtr := reflect.New(rowType)
+		rowValue := reflect.Indirect(rowValuePtr)
+		for i, col := range stmt.columns {
+			cellValue := col.Index.ValueRW(rowValue)
+			scanValues[i] = cellValue.Addr().Interface()
+		}
+		err = rows.Scan(scanValues...)
+		if err != nil {
+			return err
+		}
+		if isPtr {
+			sliceValue.Set(reflect.Append(sliceValue, rowValuePtr))
+		} else {
+			sliceValue.Set(reflect.Append(sliceValue, rowValue))
+		}
+	}
+
+	return rows.Err()
+}
+
+func (stmt *SelectStmt) errorSliceType() error {
+	expectedType := stmt.expectedTypeString()
+	return fmt.Errorf("Expected rows to be pointer to []%s or []*%s", expectedType, expectedType)
+}
+
+func SelectRowsPrintf(format string, args ...interface{}) *SelectStmt {
+	stmt := &SelectStmt{}
+
+	for _, arg := range args {
+		if tn, ok := arg.(TableName); ok {
+			if stmt.table == nil && tn.clause.matchAny(clauseSelectFrom) {
+				stmt.table = tn.table
+			}
+		} else if cols, ok := arg.(Columns); ok {
+			if cols.clause.isInput() {
+				stmt.err = errors.New("unexpected arg to SelectRowsPrintf")
+				// input parameters for the SELECT statement
+			}
+			if cols.clause == clauseSelectColumns {
+				if stmt.table == nil {
+					stmt.table = cols.table
+				}
+				stmt.columns = append(stmt.columns, cols.filtered()...)
 			}
 		}
 	}
