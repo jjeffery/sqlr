a1fef969132df1821cd96f63d0b6b9df96dc63ce
John Jeffery <jjeffery@sp.com.au>
2016-07-10 20:43:23 +1000

    Extra godoc comments for Queryer and Execer

--- sqlf.go
+++ sqlf.go
@@ -7,6 +7,9 @@ import (
 // Execer implements a single method, Exec, which executes a
 // query without returning any rows. The args for any parameter
 // placeholders in the query.
+//
+// The *DB and *Tx types in the standard library package "database/sql"
+// both implement this interface.
 type Execer interface {
 	Exec(query string, args ...interface{}) (sql.Result, error)
 }
@@ -14,6 +17,9 @@ type Execer interface {
 // Queryer implements a single method, Query, which executes
 // a query that returns zero, one or more rows. The args are
 // for any parameter placeholders in the query.
+//
+// The *DB and *Tx types in the standard library package "database/sql"
+// both implement this interface.
 type Queryer interface {
 	Query(query string, args ...interface{}) (*sql.Rows, error)
 }
