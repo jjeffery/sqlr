0b68891767661a10b98b9d2cd3055936e7b5ff82
John Jeffery <jjeffery@sp.com.au>
2016-07-16 16:34:36 +1000

    Add ConventionSnake, ConventionSame

--- convention.go
+++ convention.go
@@ -1,5 +1,7 @@
 package sqlstmt
 
+import "github.com/jjeffery/sqlstmt/private/colname"
+
 // Convention provides naming convention methods for
 // inferring a database column name from Go struct field names.
 type Convention interface {
@@ -12,4 +14,16 @@ type Convention interface {
 	// structures. The column name will be based on the name of
 	// the Go struct field and its enclosing embedded struct fields.
 	Join(prefix, name string) string
+}
+
+// ConventionSnake is the default, 'snake_case' naming convention
+var ConventionSnake Convention
+
+// ConventionSame is a naming convention where the column name
+// is identical to the Go struct field name.
+var ConventionSame Convention
+
+func init() {
+	ConventionSnake = colname.Snake
+	ConventionSame = colname.Same
 }
