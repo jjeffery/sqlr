ace648a05601ca2687ebcb1876678220816573af
John Jeffery <jjeffery@sp.com.au>
2016-07-16 22:35:45 +1000

    More tests for errors

--- stmterrs_test.go
+++ stmterrs_test.go
@@ -4,15 +4,34 @@ package sqlstmt
 
 import (
 	"database/sql"
+	"errors"
 	"testing"
 )
 
-type FakeExecer struct{}
+type FakeExecer struct {
+	execErr         error
+	rowsAffected    int64
+	rowsAffectedErr error
+	lastInsertId    int64
+	lastInsertIdErr error
+}
 
 func (fe *FakeExecer) Exec(query string, args ...interface{}) (sql.Result, error) {
-	return nil, nil
+	if fe.execErr != nil {
+		return nil, fe.execErr
+	}
+	return fe, nil
 }
-func TestCannotSetAutoIncrement(t *testing.T) {
+
+func (fe *FakeExecer) LastInsertId() (int64, error) {
+	return fe.lastInsertId, fe.lastInsertIdErr
+}
+
+func (fe *FakeExecer) RowsAffected() (int64, error) {
+	return fe.rowsAffected, fe.rowsAffectedErr
+}
+
+func TestInsert_CannotSetAutoIncrement(t *testing.T) {
 	type Row struct {
 		ID   int64 `sql:"primary key autoincrement"`
 		Name string
@@ -22,6 +41,38 @@ func TestCannotSetAutoIncrement(t *testing.T) {
 
 	err := stmt.Exec(execer, Row{})
 	if err == nil || err.Error() != "cannot set auto-increment value for type Row" {
+		t.Errorf("err=%v", err)
+	}
+}
+
+func TestInsert_ExecerExecFails(t *testing.T) {
+	type Row struct {
+		ID   int64 `sql:"primary key autoincrement"`
+		Name string
+	}
+	stmt := NewInsertRowStmt(Row{}, "tablename")
+	execer := &FakeExecer{
+		execErr: errors.New("test error condition"),
+	}
+
+	err := stmt.Exec(execer, &Row{})
+	if err == nil || err.Error() != "test error condition" {
+		t.Errorf("err=%q", err)
+	}
+}
+
+func TestInsert_LastInsertIdFails(t *testing.T) {
+	type Row struct {
+		ID   int64 `sql:"primary key autoincrement"`
+		Name string
+	}
+	stmt := NewInsertRowStmt(Row{}, "tablename")
+	execer := &FakeExecer{
+		lastInsertIdErr: errors.New("test: LastInsertId"),
+	}
+
+	err := stmt.Exec(execer, &Row{})
+	if err == nil || err.Error() != "test: LastInsertId" {
 		t.Errorf("err=%q", err)
 	}
 }
