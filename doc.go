95308d213991feb360c30b45733babf242f34854
John Jeffery <jjeffery@sp.com.au>
2016-07-16 12:14:43 +1000

    removed old sqlf package files

--- doc.go
+++
@@ -1,9 +0,0 @@
-/*
-Package sqlf provides assistance for writing and executing SQL statements.
-
-It is intended for programmers who are comfortable with
-writing SQL, but would like assistance with the sometimes tedious
-process of preparing SELECT, INSERT, UPDATE and DELETE statements
-for tables that have a large number of columns.
-*/
-package sqlf
--- stmt_test.go
+++
@@ -1,9 +0,0 @@
-package sqlf
-
-func (stmt *InsertRowStmt) Query() string {
-	return stmt.query
-}
-
-func (stmt *UpdateRowStmt) Query() string {
-	return stmt.query
-}
