1cd958b8cd6b48081877b458ba498fd52cd73726
John Jeffery <jjeffery@sp.com.au>
2017-01-28 16:14:47 +1000

    Godoc comments.

---
+++ private/codegen/doc.go
@@ -0,0 +1,4 @@
+// Package codegen provides the code-generation functionality
+// used by the sqlrow-gen tool.
+package codegen
+
--- private/private.go
+++ private/private.go
@@ -1,3 +1,8 @@
 // Package private and subdirectories have
 // no backward compatibility guarantees.
+//
+// This package is intentionally not named "internal",
+// which would prevent access outside the sqlrow package.
+// These packages are available for use, it's just that 
+// their public API is subject to change.
 package private
