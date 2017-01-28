d113f891805bac0344c083db4a7316ab5d271596
John Jeffery <jjeffery@sp.com.au>
2017-01-28 11:23:29 +1000

    Add TagInfo type and ParseTag function.
    This is used by the code generator: code duplication exists and needs to be removed. Also needs some tests.

--- private/column/info.go
+++ private/column/info.go
@@ -111,9 +111,83 @@ func newScanner(tag reflect.StructTag) *scanner.Scanner {
 				"identity",
 				"version",
 				"json",
-				"jsonb")
+				"jsonb",
+				"natural",
+				"natural_key")
 			return scan
 		}
 	}
 	return nil
+}
+
+// TagInfo is information obtained about a column from the
+// struct tags of its corresponding field.
+type TagInfo struct {
+	Ignore        bool
+	Name          string
+	PrimaryKey    bool
+	AutoIncrement bool
+	Version       bool
+	JSON          bool
+	NaturalKey    bool
+}
+
+// ParseTag returns a TagInfo containing information obtained from the
+// StructTag of the field associated with the column.
+func ParseTag(tag reflect.StructTag) TagInfo {
+	var tagInfo TagInfo
+
+	scan := newScanner(tag)
+	if scan == nil {
+		return tagInfo
+	}
+	var hadKeyword bool
+	for scan.Scan() {
+		tok, lit := scan.Token(), scan.Text()
+		switch tok {
+		case scanner.KEYWORD:
+			hadKeyword = true
+			switch strings.ToLower(lit) {
+			case "pk", "primary_key":
+				tagInfo.PrimaryKey = true
+			case "autoincrement", "autoincr":
+				tagInfo.AutoIncrement = true
+			case "primary":
+				if scan.Scan(); strings.ToLower(scan.Text()) == "key" {
+					tagInfo.PrimaryKey = true
+				}
+			case "auto":
+				if scan.Scan(); strings.ToLower(scan.Text()) == "increment" {
+					tagInfo.AutoIncrement = true
+				}
+			case "identity":
+				tagInfo.AutoIncrement = true
+			case "version":
+				tagInfo.Version = true
+			case "json", "jsonb":
+				tagInfo.JSON = true
+			case "natural_key":
+				tagInfo.NaturalKey = true
+			case "natural":
+				if scan.Scan(); strings.ToLower(scan.Text()) == "key" {
+					tagInfo.NaturalKey = true
+				}
+			}
+		case scanner.IDENT:
+			if !hadKeyword && tagInfo.Name == "" {
+				tagInfo.Name = scanner.Unquote(lit)
+			}
+		case scanner.LITERAL:
+			if !hadKeyword && tagInfo.Name == "" && scanner.IsQuoted(lit) {
+				// a string literal is accepted as the column name
+				tagInfo.Name = scanner.Unquote(lit)
+			}
+		case scanner.OP:
+			if !hadKeyword && tagInfo.Name == "" && lit == "-" {
+				// indicates should not be a column
+				tagInfo.Ignore = true
+			}
+		}
+	}
+	return tagInfo
 }
