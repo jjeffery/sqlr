8ad849f01cc1b223508301156a94ff6e29fb3428
John Jeffery <jjeffery@sp.com.au>
2017-01-28 11:30:07 +1000

    Implemented sqlrow-gen generator.
    More testing required, but minimal functionality working.

--- private/gen/gen.go
+++ private/gen/gen.go
@@ -1,8 +1,18 @@
 package gen
 
 import (
-	"errors"
+	"bytes"
 	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"reflect"
+	"strings"
+	"unicode"
+
+	"github.com/jjeffery/errors"
+	"github.com/jjeffery/sqlrow/private/colname"
+	"github.com/jjeffery/sqlrow/private/column"
 )
 
 // Model contains all of the information required by the template
@@ -36,6 +46,7 @@ type QueryType struct {
 	Singular        string // Describes one instance in error msg
 	Plural          string // Describes multiple instances in error msg
 	DBField         string // Name of the field of type sqlrow.DB (probably db)
+	SchemaField     string // Name of the schema field of type sqlrow.Schema (probably schema)
 	RowType         *RowType
 	Method          struct {
 		Get       bool
@@ -51,14 +62,303 @@ type QueryType struct {
 // RowType contains all the information the template needs about
 // a struct type that is used to represent a single DB table row.
 type RowType struct {
-	Name     string
-	IDArgs   string // for function arguments specifying primary key ID field(s)
-	IDParams string // for function parameters specifying primary key ID field(s)
-	Keyvals  string // for error messages
+	Name      string
+	IDArgs    string   // for function arguments specifying primary key ID field(s)
+	IDParams  string   // for function parameters specifying primary key ID field(s)
+	IDKeyvals string   // for log messages specifying primary key ID field(s)
+	LogProps  []string // for error messages
 }
 
 // Parse the file, and any other related files and build the
 // model, which can be used to generate the code.
 func Parse(filename string) (*Model, error) {
-	return nil, errors.New("not implemented")
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, filename, nil, 0)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot parse file").With(
+			"filename", filename,
+		)
+	}
+
+	model := &Model{
+		Package: file.Name.Name,
+	}
+	ir, err := newImportResolver(file.Imports)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, decl := range file.Decls {
+		if genDecl, ok := decl.(*ast.GenDecl); ok {
+			for _, spec := range genDecl.Specs {
+				if typeSpec, ok := spec.(*ast.TypeSpec); ok {
+					structType, ok := typeSpec.Type.(*ast.StructType)
+					if !ok {
+						continue
+					}
+					queryType, err := newQueryType(file, ir, typeSpec, structType)
+					if err != nil {
+						return nil, err
+					}
+					if queryType != nil {
+						model.QueryTypes = append(model.QueryTypes, queryType)
+					}
+				}
+			}
+		}
+	}
+	model.Imports = ir.Imports()
+
+	return model, nil
+}
+
+func newQueryType(file *ast.File, ir *importResolver, typeSpec *ast.TypeSpec, structType *ast.StructType) (*QueryType, error) {
+	var rowTypeField *ast.Field
+	var dbField *ast.Field
+	var schemaField *ast.Field
+	var methods string
+	var tableName string
+	var singular string
+	var plural string
+
+	const rowTypeFieldName = "rowType"
+	const dbTypeName = "sqlrow.DB"
+	const schemaTypeName = "sqlrow.Schema"
+
+	// Use a local import resolver for resolving field type names that will not
+	// be used in generated code.
+	localIR, err := newImportResolver(file.Imports)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, field := range structType.Fields.List {
+		fieldTypeName := localIR.exprString(field.Type)
+		if field.Tag != nil {
+			tag := reflect.StructTag(stripQuotes(field.Tag.Value))
+			if v := tag.Get("methods"); v != "" {
+				methods = v
+			}
+			if v := tag.Get("table"); v != "" {
+				tableName = v
+			}
+			if v := tag.Get("singular"); v != "" {
+				singular = v
+			}
+			if v := tag.Get("plural"); v != "" {
+				plural = v
+			}
+		}
+		if fieldTypeName == dbTypeName {
+			dbField = field
+			continue
+		}
+		if fieldTypeName == schemaTypeName {
+			schemaField = field
+			continue
+		}
+		for _, name := range field.Names {
+			if name.Name == rowTypeFieldName {
+				rowTypeField = field
+				continue
+			}
+		}
+	}
+	if rowTypeField == nil {
+		// not a struct defining a set of queries
+		return nil, nil
+	}
+	if dbField == nil {
+		return nil, errors.New("missing field").With(
+			"struct", typeSpec.Name.Name,
+			"type", dbTypeName,
+		)
+	}
+	if schemaField == nil {
+		return nil, errors.New("missing field").With(
+			"struct", typeSpec.Name.Name,
+			"type", schemaTypeName,
+		)
+	}
+
+	if tableName == "" {
+		tableName = colname.Snake.ColumnName(typeSpec.Name.Name)
+		tableName = toPlural(tableName)
+	}
+
+	if singular == "" {
+		singular = typeSpec.Name.Name
+	}
+
+	if plural == "" {
+		plural = toPlural(singular)
+	}
+
+	if methods == "" {
+		methods = "get,select,selectOne,insert,update,delete,upsert"
+	}
+
+	// at this point we have a struct that describes a query type
+	queryType := &QueryType{
+		TypeName:        typeSpec.Name.Name,
+		QuotedTableName: fmt.Sprintf("%q", tableName),
+		Singular:        singular,
+		Plural:          plural,
+		DBField:         dbField.Names[0].Name,
+		SchemaField:     schemaField.Names[0].Name,
+	}
+	for _, method := range strings.Split(methods, ",") {
+		lmethod := strings.ToLower(strings.TrimSpace(method))
+		switch lmethod {
+		case "get":
+			queryType.Method.Get = true
+		case "select":
+			queryType.Method.Select = true
+		case "selectone":
+			queryType.Method.SelectOne = true
+		case "insert":
+			queryType.Method.Insert = true
+		case "update":
+			queryType.Method.Update = true
+		case "upsert":
+			queryType.Method.Upsert = true
+		case "delete":
+			queryType.Method.Delete = true
+		default:
+			return nil, errors.New("unknown method").With(
+				"method", method,
+			)
+		}
+	}
+
+	rowType, err := newRowType(file, ir, rowTypeField.Type)
+	if err != nil {
+		return nil, err
+	}
+
+	queryType.RowType = rowType
+	return queryType, nil
+}
+
+func toPlural(s string) string {
+	return s + "s"
+}
+
+func stripRowTypeExpr(expr ast.Expr) ast.Expr {
+	for {
+		switch v := expr.(type) {
+		case *ast.Ident:
+			return v
+		case *ast.SelectorExpr:
+			return v
+		case *ast.StarExpr:
+			expr = v.X
+		case *ast.ParenExpr:
+			expr = v.X
+		default:
+			return nil
+		}
+	}
+}
+
+func newRowType(file *ast.File, ir *importResolver, typeExpr ast.Expr) (*RowType, error) {
+	typeExpr = stripRowTypeExpr(typeExpr)
+	if typeExpr == nil {
+		return nil, errors.New("unexpected type for rowType")
+	}
+	if _, ok := typeExpr.(*ast.SelectorExpr); ok {
+		return nil, errors.New("doesn't handle row types in other packages yet, row type needs to be in the same package and file")
+	}
+	rowTypeIdent, ok := typeExpr.(*ast.Ident)
+	if !ok {
+		// should not get here, checked earlier
+		return nil, errors.New("unexpected row type")
+	}
+
+	rowTypeName := rowTypeIdent.Name
+	structType := findStructType(file, rowTypeName)
+	if structType == nil {
+		return nil, errors.New("cannot find row type").With(
+			"name", rowTypeName,
+		)
+	}
+
+	var pkParams []string
+	var pkKeyvals []string
+	var pkArgs []string
+	var kvArgs []string
+
+	for _, field := range structType.Fields.List {
+		var tagInfo column.TagInfo
+		if field.Tag != nil {
+			tag := reflect.StructTag(stripQuotes(field.Tag.Value))
+			tagInfo = column.ParseTag(tag)
+		}
+		if tagInfo.Ignore {
+			continue
+		}
+		if tagInfo.PrimaryKey {
+			for _, fieldName := range field.Names {
+				paramName := lowerCaseField(fieldName.Name)
+				pkArgs = append(pkArgs, paramName)
+				pkKeyvals = append(pkKeyvals, fmt.Sprintf("%q", paramName))
+				pkKeyvals = append(pkKeyvals, paramName)
+				kvArgs = append(kvArgs, fieldName.Name)
+				typeName := ir.exprString(field.Type)
+				pkParams = append(pkParams, fmt.Sprintf("%s %s", paramName, typeName))
+			}
+		}
+		if tagInfo.NaturalKey {
+			for _, ident := range field.Names {
+				kvArgs = append(kvArgs, ident.Name)
+			}
+		}
+	}
+
+	rowType := &RowType{
+		Name:      rowTypeName,
+		IDParams:  strings.Join(pkParams, ", "),
+		IDArgs:    strings.Join(pkArgs, ", "),
+		IDKeyvals: strings.Join(pkKeyvals, ", ") + ",",
+		LogProps:  kvArgs,
+	}
+
+	return rowType, nil
+}
+
+func findStructType(file *ast.File, name string) *ast.StructType {
+	for _, decl := range file.Decls {
+		if genDecl, ok := decl.(*ast.GenDecl); ok {
+			for _, spec := range genDecl.Specs {
+				if typeSpec, ok := spec.(*ast.TypeSpec); ok {
+					structType, ok := typeSpec.Type.(*ast.StructType)
+					if !ok {
+						continue
+					}
+					if typeSpec.Name.Name == name {
+						return structType
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func stripQuotes(s string) string {
+	return strings.Trim(s, "`")
+}
+
+func lowerCaseField(s string) string {
+	var buf bytes.Buffer
+	var metLower bool
+	for _, ch := range s {
+		if !metLower && unicode.IsUpper(ch) {
+			buf.WriteRune(unicode.ToLower(ch))
+			continue
+		}
+		metLower = true
+		buf.WriteRune(ch)
+	}
+	return buf.String()
 }
---
+++ private/gen/import_resolver.go
@@ -0,0 +1,171 @@
+package gen
+
+import (
+	"fmt"
+	"go/ast"
+	"path"
+	"strings"
+)
+
+type importResolver struct {
+	imports []*ast.ImportSpec
+	used    map[string]*Import
+}
+
+func (r *importResolver) Resolve(name string) *Import {
+	if imp, ok := r.used[name]; ok {
+		return imp
+	}
+
+	// strips the quotes from the import path and returns the base name
+	pathBase := func(p string) string {
+		return path.Base(strings.TrimPrefix(strings.TrimSuffix(p, `"`), `"`))
+	}
+	// strips the quotes from the import path and returns the base name without any extension
+	// (good for import paths like "gopkg.in/xyz/abc.v1")
+	pathBaseWithoutExtension := func(p string) string {
+		p = pathBase(p)
+		return strings.TrimSuffix(p, path.Ext(p))
+	}
+
+	tests := []func(*ast.ImportSpec) bool{
+		// import has matching explicit name
+		func(is *ast.ImportSpec) bool {
+			return is.Name != nil && is.Name.Name == name
+		},
+		// import has matching import base name
+		func(is *ast.ImportSpec) bool {
+			if is.Name != nil {
+				return false
+			}
+			return pathBase(is.Path.Value) == name
+		},
+		// import has matching import base name without extension
+		func(is *ast.ImportSpec) bool {
+			if is.Name != nil {
+				return false
+			}
+			return pathBaseWithoutExtension(is.Path.Value) == name
+		},
+		// import base name contains the string somewhere
+		func(is *ast.ImportSpec) bool {
+			if is.Name != nil {
+				return false
+			}
+			return strings.Contains(pathBase(is.Path.Value), name)
+		},
+	}
+	// Search for an import whose name matches.
+	for _, test := range tests {
+		for _, importSpec := range r.imports {
+			if test(importSpec) {
+				imp := &Import{
+					Path: importSpec.Path.Value,
+				}
+				if importSpec.Name != nil {
+					imp.Name = importSpec.Name.Name
+				}
+
+				r.used[name] = imp
+				return imp
+			}
+		}
+	}
+	return nil
+}
+
+func (r *importResolver) exprString(t ast.Expr) string {
+	if t == nil {
+		return ""
+	}
+	switch v := t.(type) {
+	case *ast.BadExpr:
+		return "<bad-expr>"
+	case *ast.Ident:
+		return v.Name
+	case *ast.Ellipsis:
+		return fmt.Sprintf("...%s", r.exprString(v.Elt))
+	case *ast.BasicLit:
+		// does not appear in method declarations
+		return v.Value
+	case *ast.FuncLit:
+		notExpecting("FuncLit")
+	case *ast.CompositeLit:
+		notExpecting("CompositeLit")
+	case *ast.ParenExpr:
+		return fmt.Sprintf("(%s)", r.exprString(v.X))
+	case *ast.SelectorExpr:
+		r.Resolve(r.exprString(v.X))
+		return fmt.Sprintf("%s.%s", r.exprString(v.X), v.Sel.Name)
+	case *ast.IndexExpr:
+		return fmt.Sprintf("%s[%s]", r.exprString(v.X), r.exprString(v.Index))
+	case *ast.SliceExpr:
+		if v.Slice3 {
+			return fmt.Sprintf("%s[%s:%s]", r.exprString(v.X), r.exprString(v.Low), r.exprString(v.High))
+		}
+		return fmt.Sprintf("%s[%s:%s:%s]", r.exprString(v.X), r.exprString(v.Low), r.exprString(v.High), r.exprString(v.Max))
+	case *ast.TypeAssertExpr:
+		notExpecting("TypeAssertExpr")
+	case *ast.CallExpr:
+		notExpecting("CallExpr")
+	case *ast.StarExpr:
+		return fmt.Sprintf("*%s", r.exprString(v.X))
+	case *ast.UnaryExpr:
+		return fmt.Sprintf("%s%s", v.Op.String(), r.exprString(v.X))
+	case *ast.BinaryExpr:
+		return fmt.Sprintf("%s %s %s", r.exprString(v.X), v.Op.String(), r.exprString(v.Y))
+	case *ast.KeyValueExpr:
+		return fmt.Sprintf("%s: %s", r.exprString(v.Key), r.exprString(v.Value))
+	case *ast.ArrayType:
+		return fmt.Sprintf("[%s]%s", r.exprString(v.Len), r.exprString(v.Elt))
+	case *ast.StructType:
+		notImplemented("StructType")
+	case *ast.FuncType:
+		notImplemented("FuncType")
+	case *ast.InterfaceType:
+		notImplemented("InterfaceType")
+	case *ast.MapType:
+		return fmt.Sprintf("map[%s]%s", r.exprString(v.Key), r.exprString(v.Value))
+	case *ast.ChanType:
+		switch v.Dir {
+		case ast.SEND:
+			return fmt.Sprintf("chan<- %s", r.exprString(v.Value))
+		case ast.RECV:
+			return fmt.Sprintf("<-chan %s", r.exprString(v.Value))
+		default:
+			return fmt.Sprintf("chan %s", r.exprString(v.Value))
+		}
+	}
+
+	panic(fmt.Sprintf("unknown ast.Expr: %v", t))
+}
+
+func (r *importResolver) Imports() []*Import {
+	var imports []*Import
+	for _, imp := range r.used {
+		imports = append(imports, imp)
+	}
+	return imports
+}
+
+func newImportResolver(imports []*ast.ImportSpec) (*importResolver, error) {
+	for _, importSpec := range imports {
+		if importSpec.Name != nil && importSpec.Name.Name == "." {
+			return nil, fmt.Errorf("dot imports are not supported: . %v", importSpec.Path.Value)
+		}
+	}
+	return &importResolver{
+		imports: imports,
+		used:    make(map[string]*Import),
+	}, nil
+}
+
+func notExpecting(nodeType string) {
+	msg := fmt.Sprintf("not expecting node type of %s", nodeType)
+	panic(msg)
+}
+
+func notImplemented(nodeType string) {
+	msg := fmt.Sprintf("handling of node type not implemented: %s", nodeType)
+	panic(msg)
+}
