e71c38dba25472e28741da6fadf169790d95f202
John Jeffery <jjeffery@sp.com.au>
2018-12-14 16:22:49 +1000

    Remove deprecated DB interface.

--- querier.go
+++ querier.go
@@ -8,6 +8,7 @@ import (
 // The Querier interface defines the SQL database access methods used by this package.
 //
 // The *DB, *Tx and *Conn types in the standard library package "database/sql" all implement this interface.
+// This interface is based on https://godoc.org/github.com/golang-sql/sqlexp#Querier.
 type Querier interface {
 	// ExecContext executes a query without returning any rows.
 	// The args are for any placeholder parameters in the query.
@@ -18,30 +19,8 @@ type Querier interface {
 	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
 }
 
-// The DB interface defines the SQL database access methods used by this package.
-//
-// Deprecated: use Querier instead.
-type DB interface {
-	Querier
-
-	// Exec executes a query without returning any rows.
-	// The args are for any placeholder parameters in the query.
-	//
-	// Deprecated: use ExecContext instead
-	Exec(query string, args ...interface{}) (sql.Result, error)
-
-	// Query executes a query that returns rows, typically a SELECT.
-	// The args are for any placeholder parameters in the query.
-	//
-	// Deprecated: use QueryContext instead.
-	Query(query string, args ...interface{}) (*sql.Rows, error)
-}
-
 var (
 	_ Querier = &sql.DB{}
 	_ Querier = &sql.Tx{}
 	_ Querier = &sql.Conn{}
-
-	_ DB = &sql.DB{}
-	_ DB = &sql.Tx{}
 )
